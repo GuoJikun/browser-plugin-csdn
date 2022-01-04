669aa403c3b116126d53975dee0b6115d49ee492
GuoJikun <guojikun@y-open.com>
2022-01-04 17:33:57 +0800

    feat:优化

--- content_scripts/csdn.js
+++ content_scripts/csdn.js
@@ -1,3 +1,8 @@
+function removeLoginBox() {}
+
+document.querySelector(".passport-login-container").style.display =
+  "none !important";
+
 document.querySelectorAll("pre code").forEach((c) => {
   c.contentEditable = "true";
 });
