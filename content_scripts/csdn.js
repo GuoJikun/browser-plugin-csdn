903cfeb0a29d242ffa53dd863da8617ab4e8f78d
GuoJikun <guojikun@y-open.com>
2022-01-07 17:14:16 +0800

    feat:增加禁用登录的自动弹窗

--- content_scripts/csdn.js
+++ content_scripts/csdn.js
@@ -1,7 +1,16 @@
-function removeLoginBox() {}
-
-document.querySelector(".passport-login-container").style.display =
-  "none !important";
+function HideLoginBox() {
+  const styleEl = document.createElement("style");
+  styleEl.setAttribute("type", "text/css");
+  const style = `div.passport-container.passport-container-mini { 
+    display:none;
+  }
+  body{color: red !important;}`;
+  styleEl.innerHTML = style;
+  document.head.appendChild(styleEl);
+  console.log(123);
+  //  document.getElementsByTagName('head').item(0).appendChild(style);
+}
+HideLoginBox();
 
 document.querySelectorAll("pre code").forEach((c) => {
   c.contentEditable = "true";
